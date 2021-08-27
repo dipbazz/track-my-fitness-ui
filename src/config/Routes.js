821850f59007fea756479ab82de44e44cd7b56f4
Dipesh Bajgain <dipbazz@gmail.com>
2021-08-27 16:32:46 +0545

    Add higher order component to check for authentication

--- src/config/Routes.js
+++ src/config/Routes.js
@@ -1,6 +1,6 @@
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Home from '../components/Home';
-import ProtectedRoute from '../utils/ProtectedRoute';
 import LoginContainer from '../containers/LoginContainer';
 import RegisterContainer from '../containers/RegisterContainer';
 import MeasurementContainer from '../containers/MeasurementContainer';
@@ -9,26 +9,27 @@ import MeasurementDetailContainer from '../containers/MeasurementDetailContainer
 import ProgressContainer from '../containers/ProgressContainer';
 import MoreContainer from '../containers/MoreContainer';
 
+/* eslint-disable react/jsx-props-no-spreading */
+const withAuthentication = (WrappedComponent) => {
+  const ProtectedRoute = (props) => {
+    const isAuthenticated = useSelector((state) => state.currentUser.isAuthenticated);
+    if (!isAuthenticated) {
+      return <Redirect to={{ pathname: '/login' }} />;
+    }
+    return <WrappedComponent {...props} />;
+  };
+  ProtectedRoute.displayName = 'ProtectedRoute';
+  return ProtectedRoute;
+};
+
 const Routes = () => (
   <Switch>
-    <ProtectedRoute path="/" exact>
-      <Home />
-    </ProtectedRoute>
-    <ProtectedRoute path="/measurement" exact>
-      <MeasurementContainer />
-    </ProtectedRoute>
-    <ProtectedRoute path="/measurement/list" exact>
-      <MeasurementListContainer />
-    </ProtectedRoute>
-    <ProtectedRoute path="/measurement/detail/:date" exact>
-      <MeasurementDetailContainer />
-    </ProtectedRoute>
-    <ProtectedRoute path="/progress" exact>
-      <ProgressContainer />
-    </ProtectedRoute>
-    <ProtectedRoute path="/more" exact>
-      <MoreContainer />
-    </ProtectedRoute>
+    <Route path="/" exact component={withAuthentication(Home)} />
+    <Route path="/measurement" exact component={withAuthentication(MeasurementContainer)} />
+    <Route path="/measurement/list" exact component={withAuthentication(MeasurementListContainer)} />
+    <Route path="/measurement/detail/:date" exact component={withAuthentication(MeasurementDetailContainer)} />
+    <Route path="/progress" exact component={withAuthentication(ProgressContainer)} />
+    <Route path="/more" exact component={withAuthentication(MoreContainer)} />
     <Route path="/login" component={LoginContainer} exact />
     <Route path="/register" component={RegisterContainer} exact />
   </Switch>
